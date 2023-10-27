9e178f7d5d25a584c63fd1bb3eaca32088081870
Shubhamctrl2 <2022hs70025@wilp.bits-pilani.ac.in>
2023-10-27 15:55:23 +0200

    CORS BLOCKER

--- src/Components/Pages/Registration.jsx
+++ src/Components/Pages/Registration.jsx
@@ -1,19 +1,75 @@
+import { useState } from 'react';
 import registerSvg from '../../assets/images/registerSVG.svg'
+import axios from 'axios';
+
 const Registration = () =>
 {
+const [formData, setFormData] = useState([]);
+
+const handleInputChange = (event) =>{
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+}
+
+const onSubmitHandler = (e) =>{
+  e.preventDefault();
+  console.log('clicked', formData);
+  axios.post('http://localhost:8080/signup',{
+    
+      "firstName" : formData.firstname,
+      "lastName": formData.lastname,
+      "username" : formData.email,
+      "password" : formData.password
+  
+  },{
+    headers :  {
+      'Content-Type': 'application/json',
+      'Access-Control-origin' : "*"
+    },
+    withCredentials : true
+  }).then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+
+console.log(formData)
 return (
     <div>
        <div className="container">
       <form>
         <h2 className='text-center'>Sign Up</h2>
         <div className="form-group">
+          <label className='mb-2'>First Name </label>
+          <input
+            type="text"
+            className="form-control"
+            name="firstname"
+            value={formData.firstname}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label className='mb-2'>Last Name</label>
+          <input
+            type="lastname"
+            className="form-control"
+            name="lastname"
+            value={formData.lastname}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div className="form-group">
           <label className='mb-2'>Email</label>
           <input
             type="email"
             className="form-control"
             name="email"
-            // value={formData.email}
-            // onChange={handleInputChange}
+            value={formData.email}
+            onChange={handleInputChange}
             required
           />
         </div>
@@ -23,24 +79,24 @@ return (
             type="password"
             className="form-control"
             name="password"
-            // value={formData.password}
-            // onChange={handleInputChange}
+            value={formData.password}
+            onChange={handleInputChange}
             required
           />
         </div>
-        <div className="form-group">
+        {/* <div className="form-group">
           <label className='my-2'>Confirm Password</label>
           <input
             type="password"
             className="form-control"
             name="confirmPassword"
-            // value={formData.confirmPassword}
-            // onChange={handleInputChange}
+            value={formData.confirmPassword}
+            onChange={handleInputChange}
             required
           />
-        </div>
+        </div> */}
         <div style={{display:'flex', justifyContent:'center'}}>
-        <button type="submit" className="btn mt-3 content-center btn-primary">
+        <button type="submit" onClick ={onSubmitHandler} className="btn mt-3 content-center btn-primary">
           Register
         </button>
         </div>
